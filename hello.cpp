2413ca75d73f47c03b200e3eaff09b935d62a0a3
longbp3477-cmyk <longbp3477@ut.edu.vn>
2026-09-22 01:17:07 +0700

    Day 1: C++ basic input output

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "Hello Long!" << endl;
+    string name;
+
+    cout << "Nhap ten cua ban: ";
+    cin >> name;
+
+    cout << "Hello " << name << "!" << endl;
+
     return 0;
-}   
+}
